3a95c3abed0e33cb78523783514dd023a99bf076
umuhozalionel <muhozalionel@gmail.com>
2024-12-13 17:24:28 -0800

    Initial commit

---
+++ controllers/AppController.js
@@ -0,0 +1,16 @@
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  }
+
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.status(200).json({ users, files });
+  }
+}
+
+module.exports = AppController;
